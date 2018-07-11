410dcadae0757d1a133c64b66f131d92964b0b3e
Elie Khoury <elie.kh@gmail.com>
2018-07-11 22:23:35 +0200

    added a server component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
 import { latLng, tileLayer, DomUtil, Control, Map, LayerOptions } from 'leaflet';
 import { LeafletDirective } from '@asymmetrik/ngx-leaflet';
+import { Observable } from 'rxjs/internal/Observable';
+import { HttpClient, HttpRequest, HttpEventType, HttpResponse } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +11,7 @@ import { LeafletDirective } from '@asymmetrik/ngx-leaflet';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  private current_map:Map;
+  private current_map: Map;
 
   options = {
     layers: [
@@ -23,22 +26,26 @@ export class AppComponent {
     position: 'topleft'
   });
 
+  constructor(private httpClient: HttpClient) {
+
+  }
+
   ngOnInit() {
     this.uploadControl.onAdd = (mapbox: Map) => {
       let container: HTMLElement = DomUtil.create('label', 'leaflet-bar leaflet-control');
       container.style.backgroundColor = 'white';
       container.style.width = '30px';
       container.style.height = '30px';
-      container.style.backgroundImage ='url(/assets/upload.png)';
-      container.style.backgroundSize='cover';
-      container.style.backgroundRepeat='no-repeat';
-      container.style.backgroundPosition='center center';
-      container.style.cursor='pointer';
-      container.setAttribute('for','data_upload');
+      container.style.backgroundImage = 'url(/assets/upload.png)';
+      container.style.backgroundSize = 'cover';
+      container.style.backgroundRepeat = 'no-repeat';
+      container.style.backgroundPosition = 'center center';
+      container.style.cursor = 'pointer';
+      container.setAttribute('for', 'data_upload');
       return container;
     };
 
-    
+
   }
 
   onMapReady(map: Map) {
@@ -46,8 +53,18 @@ export class AppComponent {
     this.current_map.addControl(this.uploadControl);
   }
 
-  onChange(event:any) {
-    console.info('changed');
+  onChange(files: FileList) {
+    this.postFile(files[0]);
   }
+  //
 
+  postFile(fileToUpload: File) {
+    const endpoint = 'http://localhost:8000/upload';
+    const formData: FormData = new FormData();
+    formData.append('fileKey', fileToUpload, fileToUpload.name);
+    return this.httpClient
+      .post(endpoint, formData).subscribe((response)=>{
+        console.info('ok');
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     LeafletModule.forRoot()
   ],

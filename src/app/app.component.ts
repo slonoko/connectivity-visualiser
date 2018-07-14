4f54f5e4d548291118aac55d8bfc8ef01c153553
Elie Khoury <elie.kh@gmail.com>
2018-07-14 14:14:53 +0200

    added statistics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,14 +15,14 @@ export class AppComponent {
   private current_map: Map;
   private disabledFileUpload: boolean;
 
-  private mapLayer : TileLayer = tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoieGVvbm9zIiwiYSI6ImNqamwwMzhpeTFhajMzbHNvZ2ZrbXB3Z3gifQ.BcuWxZHulkyQLRZwxe8ooA',
-  { maxZoom: 18, attribution: '...', id: 'mapbox.streets-basic' });
- 
+  private mapLayer: TileLayer = tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoieGVvbm9zIiwiYSI6ImNqamwwMzhpeTFhajMzbHNvZ2ZrbXB3Z3gifQ.BcuWxZHulkyQLRZwxe8ooA',
+    { maxZoom: 18, attribution: '...', id: 'mapbox.streets-basic' });
+
   private networkOverlay: LayerGroup = new LayerGroup();
   private signalOverlay: LayerGroup = new LayerGroup();
 
   options = {
-    layers: [this.mapLayer,this.networkOverlay
+    layers: [this.mapLayer, this.networkOverlay
     ],
     zoom: 11,
     center: latLng(48.792053, 9.187813)
@@ -30,14 +30,18 @@ export class AppComponent {
 
   layersControl = {
     overlays: {
-      'Network Coverage': this.networkOverlay,
-      'Signal Coverage': this.signalOverlay
-  
+      'Network type Coverage': this.networkOverlay,
+      'Signal strength Coverage': this.signalOverlay
+
     }
   }
 
-  uploadControl: Control = new Control({
+  private uploadControl: Control = new Control({
     position: 'topleft'
+  });
+
+  private infoControl: Control = new Control({
+    position: 'bottomright'
   });
 
   constructor(private httpClient: HttpClient) {
@@ -46,50 +50,28 @@ export class AppComponent {
 
   ngOnInit() {
     this.uploadControl.onAdd = (mapbox: Map) => {
-      let container: HTMLElement = DomUtil.create('label', 'leaflet-bar leaflet-control');
-      container.style.backgroundColor = 'white';
-      container.style.width = '30px';
-      container.style.height = '30px';
-      container.style.backgroundImage = 'url(/assets/upload.png)';
-      container.style.backgroundSize = 'cover';
-      container.style.backgroundRepeat = 'no-repeat';
-      container.style.backgroundPosition = 'center center';
-      container.style.cursor = 'pointer';
+      let container: HTMLElement = DomUtil.create('label', 'leaflet-bar leaflet-control leaflet-upload-btn');
       container.setAttribute('for', 'data_upload');
+      container.setAttribute('_ngcontent-c0', '');
       return container;
     };
 
-
-  }
-
-  onLayerActivated(event:any){
-    console.info("test");
+    this.infoControl.onAdd = (map: Map) => {
+      let container: HTMLElement = DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-info-panel');
+      container.setAttribute('_ngcontent-c0', '');
+      container.style.display='none';
+      return container;
+    };
   }
 
   onMapReady(map: Map) {
     this.current_map = map;
     this.current_map.addControl(this.uploadControl);
+    this.current_map.addControl(this.infoControl);
   }
 
-  /*
-    onChange(files: FileList) {
-      this.postFile(files[0]);
-    }
-  
-    postFile(fileToUpload: File) {
-      const endpoint = 'http://localhost:8000/upload';
-      const formData: FormData = new FormData();
-      formData.append('fileKey', fileToUpload, fileToUpload.name);
-      return this.httpClient
-        .post(endpoint, formData).subscribe((response)=>{
-          console.info('ok');
-        });
-    }*/
-
   onFileSelect(input: HTMLInputElement) {
-
     const files = input.files;
-
     if (files && files.length) {
 
       console.log("Filename: " + files[0].name);
@@ -98,6 +80,8 @@ export class AppComponent {
 
       const fileToRead = files[0];
       let csvContent: any;
+
+      let statistics: any = { 'total': 0, 'net': { 'r': 0, 'y': 0, 'g': 0, 'o': 0 }, 'signal': { 'r': 0, 'y': 0, 'g': 0 } };
 
       const fileReader = new FileReader();
       fileReader.onload = (fileLoadedEvent: any) => {
@@ -116,25 +100,41 @@ export class AppComponent {
 
           if (c < csvLines.length) {
             for (let i: number = c; i < csvLines.length && i - c < 500; i++) {
+              statistics.total += 1;
               let csvLine = csvLines[i];
               let csvTabs = csvLine.split('\t');
               let coords = { 'longitude': csvTabs[1], 'latitude': csvTabs[2] };
               let net_color = 'red';
               let signal_color = 'red';
 
-              if(c==0){
+              if (c == 0) {
                 this.current_map.panTo(latLng(csvTabs[2], csvTabs[1]));
                 this.current_map.setZoom(11);
               }
 
               if (csvTabs[4] == '3' || csvTabs[4] == '4') {
                 signal_color = 'green';
+                statistics.signal.g += 1;
               } else if (csvTabs[4] == '2') {
                 signal_color = 'yellow';
+                statistics.signal.y += 1;
+              } else {
+                signal_color = 'red';
+                statistics.signal.r += 1;
               }
 
-              if (csvTabs[5] != undefined)
-              net_color = this.cellNetworkColor(csvTabs[5]);
+              if (csvTabs[5] != undefined) {
+                net_color = this.cellNetworkColor(csvTabs[5]);
+                if (net_color == 'yellow') {
+                  statistics.net.y += 1;
+                } else if (net_color == 'green') {
+                  statistics.net.g += 1;
+                } else if (net_color == 'orange') {
+                  statistics.net.o += 1;
+                } else if (net_color == 'red') {
+                  statistics.net.r += 1;
+                }
+              }
 
               if (prevCoords.longitude != undefined) {
                 if (coords.latitude != undefined && prevCoords.longitude != coords.longitude && prevCoords.latitude != coords.latitude) {
@@ -171,6 +171,8 @@ export class AppComponent {
             this.uploadControl.getContainer().style.backgroundImage = 'url(/assets/upload.png)';
             this.uploadControl.getContainer().style.backgroundColor = 'white';
             this.disabledFileUpload = false;
+            this.infoControl.getContainer().style.display='block';
+            this.infoControl.getContainer().innerHTML='<table style="width: 100%;padding: 4px;"><tr><td style="font-weight: bold;">Signal Strength</td><td style="font-weight: bold;">Network Type</td></tr><tr><td style="background-color:red;text-align:center;color:white">'+ Math.floor((statistics.signal.r/statistics.total)*100) + '%</td><td style="background-color:red;text-align:center;color:white">'+ Math.floor((statistics.net.r/statistics.total)*100) + '%</td></tr><tr><td style="background-color:yellow;text-align:center;">'+ Math.floor((statistics.signal.y/statistics.total)*100) + '%</td><td style="background-color:yellow;text-align:center;">'+ Math.floor((statistics.net.y/statistics.total)*100) + '%</td></tr><tr><td style="background-color:green;text-align:center;color:white">'+ Math.floor((statistics.signal.g/statistics.total)*100) + '%</td><td style="background-color:orange;text-align:center;color:white">'+ Math.floor((statistics.net.o/statistics.total)*100) + '%</td></tr><tr><td></td><td style="background-color:green;text-align:center;color:white">'+ Math.floor((statistics.net.g/statistics.total)*100) + '%</td></tr></table>';
             clearInterval(looper);
           }
         }, 1);
